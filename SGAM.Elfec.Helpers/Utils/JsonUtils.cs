c036b5c26a85d5db4445165ca12cf873cb85327e
diegoRodriguezAguila <diroag@gmail.com>
2016-12-14 16:38:28 -0400

    Committing untracked files

---
+++ SGAM.Elfec.Helpers/Utils/JsonUtils.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using SGAM.Elfec.Model.WebServices.Converters;
+using SGAM.Elfec.Model.WebServices.JsonContractResolver;
+using System;
+
+namespace SGAM.Elfec.Helpers.Utils
+{
+    public class JsonUtils
+    {
+        private static WeakReference<JsonSerializerSettings> _settings;
+
+        public static JsonSerializerSettings Settings
+        {
+            get
+            {
+                JsonSerializerSettings jsonSettings = null;
+                _settings?.TryGetTarget(out jsonSettings);
+                if (jsonSettings != null)
+                    return jsonSettings;
+                jsonSettings = new JsonSerializerSettings()
+                {
+                    ContractResolver = new SnakeCasePropertyNamesContractResolver(),
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+                jsonSettings.Converters.Add(new SnakeCaseEnumConverter { AllowIntegerValues = true });
+                jsonSettings.Converters.Add(new EntityConverter());
+                _settings?.SetTarget(jsonSettings);
+                if (_settings == null)
+                    _settings = new WeakReference<JsonSerializerSettings>(jsonSettings);
+                return jsonSettings;
+            }
+        }
+    }
+}
